ece860d89296a10fc249a644d76f2938305b5b3e
EthanWakeford <ethanwakeford@gmail.com>
2024-06-03 23:35:08 -0500

    added colors to hero

--- src/components/shared/sections/Hero.tsx
+++ src/components/shared/sections/Hero.tsx
@@ -42,7 +42,7 @@ const Hero: React.FC<HeroProps> = ({
   };
 
   return (
-    <div className='relative h-[92vh] bg-neutral-8 shadow-inner'>
+    <div className='relative h-[92vh] w-full bg-neutral-8 shadow-inner'>
       {/* {backgroundImages.map((image, index) => (
         <div
           key={image}
@@ -64,16 +64,24 @@ const Hero: React.FC<HeroProps> = ({
             {tagline}
           </h3>
           <div className='flex flex-col items-center justify-center gap-4 lg:flex-row lg:gap-16'>
-            <a href='#contact' className=''>
+            <a href='/contact' className=''>
               <button className='bg-other transform rounded-xl bg-primary-lt1 px-12 py-4 text-lg text-neutral-2 transition duration-300 ease-in-out hover:scale-110  hover:bg-primary-lt2'>
                 {call_to_action_text}
               </button>
             </a>
-            <p className='text-base text-neutral-2 lg:text-lg'>
+            {/* <p className='text-base text-neutral-2 lg:text-lg'>
               <a href='tel:9189407800'>Call: (918) 940-7800</a>
-            </p>
+            </p> */}
           </div>
         </div>
+      </div>
+      <div className='absolute inset-0 z-0 flex flex-col justify-end'>
+        <div className='absolute inset-0 bg-black bg-opacity-0 backdrop-blur-sm'></div>
+        <div className='h-8 w-full bg-primary-dk1'></div>
+        <div className='h-8 w-full bg-primary-md2'></div>
+        <div className='h-8 w-full bg-primary-lt1'></div>
+        <div className='h-8 w-full bg-secondary-md2'></div>
+        <div className='h-8 w-full bg-secondary-lt1'></div>
       </div>
     </div>
   );
