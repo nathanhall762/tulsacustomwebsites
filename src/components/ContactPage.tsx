ac79c07939cd28609716a072d5466582ce0a472c
EthanWakeford <ethanwakeford@gmail.com>
2024-06-03 22:37:52 -0500

    added contact page

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -1,3 +1,6 @@
+import { Icon } from '@iconify-icon/react/dist/iconify.mjs';
+import { useState, useEffect } from 'react';
+
 interface Props {
   title: string;
   subHeading: string;
@@ -17,11 +20,49 @@ const ContactPage: React.FC<Props> = ({
   phone,
   socials,
 }) => {
+  const [dotColor, setDotColor] = useState<string>('secondary-lt2');
+
+  useEffect(() => {
+    const colors = [
+      'secondary-md1',
+      'primary-dk2',
+      'primary-lt2',
+      'secondary-lt2',
+    ];
+    let colorIndex = 0;
+
+    const changeColor = () => {
+      colorIndex = (colorIndex + 1) % colors.length;
+      setDotColor(colors[colorIndex]);
+    };
+
+    const intervalId = setInterval(changeColor, 2000); // Change color every 2 seconds
+
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount
+  }, []);
+
   return (
-    <div className='flex flex-col items-center bg-ltbg1 px-8 pt-32 text-center text-black lg:pb-40 lg:pt-64 dark:bg-dkbg1 dark:text-white'>
-      <h1 className='pb-4 text-2xl lg:text-4xl'>{title}</h1>
-      <h2 className='pb-16 lg:text-xl'>{subHeading}</h2>
-      <div className='grid h-48 grid-cols-2 grid-rows-2 pb-16  lg:text-xl'>
+    <div className='flex h-screen w-screen items-center justify-center px-4 pb-[20vh] text-center text-neutral-1 dark:text-neutral-9'>
+      <div className='w-full max-w-7xl bg-neutral-9 px-4 py-16'>
+        <h1 className='bg-gradient-to-r from-secondary-dk2 to-secondary-lt2 bg-clip-text pb-4 text-4xl text-secondary-md3 text-transparent lg:text-8xl'>
+          {title}
+        </h1>
+        <h2 className='pb-16 font-body text-xl lg:text-2xl'>{subHeading}</h2>
+        <div className='flex flex-col items-center justify-center'>
+          <a
+            href={`mailto:${email}`}
+            className='text-wrap break-words pb-2 text-primary-lt2 lg:text-xl'
+          >
+            {email}
+          </a>
+          <Icon
+            className={`pb-2 text-3xl text-${dotColor} transition-colors duration-md`}
+            icon='oui:dot'
+          />
+          <p className='text-neutral-1'>Nathan & Ethan.</p>
+        </div>
+      </div>
+      {/* <div className='grid h-48 grid-cols-2 grid-rows-2 pb-16  lg:text-xl'>
         <div className='flex size-full items-center justify-center border-b border-r border-neutral-300'>
           <p className='p-4'>{email}</p>
         </div>
@@ -43,7 +84,7 @@ const ContactPage: React.FC<Props> = ({
             ></i>
           </a>
         ))}
-      </nav>
+      </nav> */}
     </div>
   );
 };
