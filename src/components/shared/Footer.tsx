e9510769ecb0b2aa694262c1fd46a02ce2755f3f
Nathan Hall <nathanhall762@gmail.com>
2024-11-09 17:09:48 -0600

    style fixes

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -13,7 +13,7 @@ interface Props {
   address?: string;
   email?: string;
   socials: { link: string; type: string }[];
-  phone: string;
+  phone?: string;
   copyright: string;
 }
 
@@ -38,41 +38,34 @@ const Footer: React.FC<Props> = ({
   };
 
   return (
-    <footer className='bg-neutral-9 pb-8 pt-16 text-center text-neutral-1 w-full overflow-x-hidden lg:px-10 dark:bg-neutral-9 dark:text-neutral-1'>
-      <div className='flex flex-col items-center justify-evenly lg:flex-row lg:justify-between size-full'>
-        <div className='hidden lg:block nav-links'>
-          {address && 
-          <a href="https://maps.app.goo.gl/7oMSLMRF64DMPTx8A" target='_blank'>
-            <p className='text-left'>
-              {address}
-              </p>
-          </a>
-            }
-          {email && 
-          <a href={`mailto:${email}`} target='_blank'>
-            <p className='text-left'>
-              {email}
-              </p>
-          </a>
-            }
-          <a href='tel:9189407800'>
-            <p className='text-left'>{formatPhoneNumber(phone)}</p>
-          </a>
+    <footer className='w-full overflow-x-hidden bg-neutral-9 pb-8 pt-16 text-center text-neutral-1 lg:px-10 dark:bg-neutral-9 dark:text-neutral-1'>
+      <div className='flex size-full flex-col items-center justify-evenly lg:flex-row lg:justify-between'>
+        <div className='nav-links hidden lg:block'>
+          {address && (
+            <a href='https://maps.app.goo.gl/7oMSLMRF64DMPTx8A' target='_blank'>
+              <p className='text-left'>{address}</p>
+            </a>
+          )}
+          {email && (
+            <a href={`mailto:${email}`} target='_blank'>
+              <p className='text-left'>{email}</p>
+            </a>
+          )}
+          {phone && (
+            <a href={`tel:${phone}`}>
+              <p className='text-left'>{formatPhoneNumber(phone)}</p>
+            </a>
+          )}
         </div>
         <div>
           <nav className='mb-8 flex flex-col items-center gap-2 lg:flex-row lg:justify-end lg:gap-8 '>
             {navs.map((nav) => (
-              <a
-                className='nav-links'
-                href={`${nav.link}`}
-                key={nav.name}
-              >
+              <a className='nav-links' href={`${nav.link}`} key={nav.name}>
                 {nav.name}
               </a>
             ))}
             <a href='/contact' className=''>
-              <Button text='Contact Us' url='/contact' size='sm'>
-              </Button>
+              <Button text='Contact Us' url='/contact' size='sm'></Button>
             </a>
           </nav>
           <nav className='mb-8 flex h-10 justify-center gap-4 lg:justify-end lg:gap-8'>
@@ -80,7 +73,7 @@ const Footer: React.FC<Props> = ({
               <a
                 href={social.link}
                 key={social.type}
-                className='text-4xl text-neutral-9 hover:text-primary-md1 dark:text-neutral-1'
+                className='hover:text-primary-md1 text-4xl text-neutral-9 dark:text-neutral-1'
               >
                 <Icon
                   icon={
--- src/components/shared/page-sections/text-components/Cta.tsx
+++ src/components/shared/page-sections/text-components/Cta.tsx
@@ -1,16 +1,15 @@
 import Button from '../../ui/Button';
 
 interface Props {
-  content: { cta: string; button_text: string; url: string };
+  content: { cta: string; button_text: string; url: string; image: string };
 }
 
 const Cta: React.FC<Props> = ({ content }) => {
-  const image = 'public/Tulsa-Custom-Websites-1.webp';
   return (
     <div className='my-40 w-full'>
       <div className='flex lg:flex-row flex-col justify-center items-center bg-border-gradient-reverse p-8 shadow-md gap-16 lg:py-12 py-16'>
       <div className='lg:size-[15%] aspect-square '>
-        <img src={image} alt="Tulsa Custom Websites Logo" className='w-full h-full object-cover' />
+        <img src={content.image} alt="Tulsa Custom Websites Logo" className='w-full h-full object-cover' />
       </div>
         <div className='flex flex-col items-center gap-8 '>
           <h2 className='font-semibold text-center text-neutral-9 lg:text-left lg:text-[9.6rem] w-fit'>
