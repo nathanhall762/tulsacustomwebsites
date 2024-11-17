c439cdb38e271f4e6fc07424e86995b9157500bf
EthanWakeford <ethanwakeford@gmail.com>
2024-11-17 15:29:49 -0600

    added  new bg images and attached them to certain components

--- src/components/shared/page-sections/text-components/BulletStatsCard.tsx
+++ src/components/shared/page-sections/text-components/BulletStatsCard.tsx
@@ -21,68 +21,70 @@ const BulletStatsCard: React.FC<Props> = ({
   link_url,
 }) => {
   return (
-    <div
-      className={`dark:text-white my-20 flex justify-center bg-cover bg-center text-neutral-9 shadow-inner lg:px-[32rem]`}
-      style={{ backgroundImage: `url('${bg_image}')` }}
-      title={image_alt_text}
-    >
-      <div className='my-[-2rem] flex h-full flex-col justify-between rounded-[2.5rem] bg-neutral-3 py-8 text-center text-neutral-8 shadow-md transition-all lg:col-span-7 lg:px-6 lg:py-16 dark:bg-neutral-7 dark:text-neutral-2'>
-        <div>
-          <div className='flex flex-col items-center'>
-            <ReactMarkdown
-              components={{
-                p(props) {
-                  const { node, ...rest } = props;
-                  return <h2 className='' {...rest} />;
-                },
-              }}
-              className=''
-            >
-              {heading}
-            </ReactMarkdown>
-            <div className='mb-16 h-[2px] w-3/4 max-w-2xl bg-border-gradient'></div>
-            <div className='px-12'>
-              {bold_text && (
+    <div className='flex w-full items-center justify-center'>
+      <div
+        className={`my-20 flex max-w-[1500px] justify-center bg-cover bg-center text-neutral-9 shadow-inner lg:px-[32rem]`}
+        style={{ backgroundImage: `url('${bg_image}')` }}
+        title={image_alt_text}
+      >
+        <div className='my-[-2rem] flex h-full flex-col justify-between rounded-[2.5rem] bg-neutral-3 py-8 text-center text-neutral-8 shadow-md transition-all lg:col-span-7 lg:px-6 lg:py-16 dark:bg-neutral-7 dark:text-neutral-2'>
+          <div>
+            <div className='flex flex-col items-center'>
+              <ReactMarkdown
+                components={{
+                  p(props) {
+                    const { node, ...rest } = props;
+                    return <h2 className='' {...rest} />;
+                  },
+                }}
+                className=''
+              >
+                {heading}
+              </ReactMarkdown>
+              <div className='mb-16 h-[2px] w-3/4 max-w-2xl bg-border-gradient'></div>
+              <div className='px-12'>
+                {bold_text && (
+                  <ReactMarkdown
+                    className='markdown text-left lg:text-left'
+                    components={{
+                      p(props) {
+                        const { node, ...rest } = props;
+                        return (
+                          <strong>
+                            <p
+                              className='h2-sub mb-12 text-center text-primary-5'
+                              {...rest}
+                            />
+                          </strong>
+                        );
+                      },
+                    }}
+                  >
+                    {bold_text}
+                  </ReactMarkdown>
+                )}
                 <ReactMarkdown
-                  className='markdown text-left lg:text-left'
+                  className='markdown mb-32 text-left lg:text-left'
                   components={{
                     p(props) {
                       const { node, ...rest } = props;
                       return (
-                        <strong>
-                          <p
-                            className='h2-sub mb-12 text-center text-primary-5'
-                            {...rest}
-                          />
-                        </strong>
+                        <p className='h3-sub mb-2 text-[2.0rem]' {...rest} />
                       );
                     },
                   }}
                 >
-                  {bold_text}
+                  {body_text}
                 </ReactMarkdown>
-              )}
-              <ReactMarkdown
-                className='markdown mb-32 text-left lg:text-left'
-                components={{
-                  p(props) {
-                    const { node, ...rest } = props;
-                    return (
-                      <p className='h3-sub mb-2 text-[2.0rem]' {...rest} />
-                    );
-                  },
-                }}
-              >
-                {body_text}
-              </ReactMarkdown>
+              </div>
             </div>
           </div>
+          {link_text && (
+            <div>
+              {link_url && <Button url={link_url} text={link_text}></Button>}
+            </div>
+          )}
         </div>
-        {link_text && (
-          <div>
-            {link_url && <Button url={link_url} text={link_text}></Button>}
-          </div>
-        )}
       </div>
     </div>
   );
--- src/components/shared/page-sections/text-components/ImageByCopy.tsx
+++ src/components/shared/page-sections/text-components/ImageByCopy.tsx
@@ -10,6 +10,7 @@ interface Props {
   link_url?: string;
   link_text?: string;
   reversed?: boolean;
+  backgroundTexture?: string;
 }
 
 const ImageByCopy: React.FC<Props> = ({
@@ -21,70 +22,81 @@ const ImageByCopy: React.FC<Props> = ({
   link_url,
   link_text,
   reversed = false,
+  backgroundTexture,
 }) => {
   return (
-    <section className={`my-24 flex justify-center lg:m-32`}>
-      <div className='flex  flex-col transition-all lg:grid lg:grid-cols-12 lg:gap-8'>
-        <img
-          src={image}
-          alt={image_alt_text}
-          className={`${reversed && 'lg:order-1'}  box-border w-screen object-cover overflow-visible z-[0] lg:col-span-5 lg:row-span-1 lg:h-full lg:w-full`}
-        />
-        <div className='z-[1] radius-3xl flex h-full flex-col justify-between bg-neutral-3 py-8 text-center text-neutral-8 shadow-md transition-all lg:col-span-7 lg:px-6 lg:py-16 dark:bg-neutral-7 dark:text-neutral-2'>
-          <div>
-            <div className='flex flex-col items-center'>
-              <ReactMarkdown
-                components={{
-                  p(props) {
-                    const { node, ...rest } = props;
-                    return <h2 className='' {...rest} />;
-                  },
-                }}
-                className=''
-              >
-                {heading}
-              </ReactMarkdown>
-              <div className='mb-16 h-[2px] w-3/4 max-w-2xl bg-border-gradient'></div>
-              <div className='px-12'>
-                {bold_text && (
-                  <ReactMarkdown
-                    className='markdown text-left lg:text-left'
-                    components={{
-                      p(props) {
-                        const { node, ...rest } = props;
-                        return <strong><p className='h2-sub mb-12 text-center text-primary-5' {...rest} /></strong>;
-                      },
-                    }}
-                  >
-                    {bold_text}
-                  </ReactMarkdown>
-                )}
+    <section className='relative m-40 flex w-full justify-center'>
+      <div
+        className='absolute inset-0 -z-10 bg-cover bg-center'
+        style={{
+          backgroundImage: `url(${backgroundTexture})`,
+        }}
+      ></div>
+      <div className={`my-24 max-w-[1500px]  lg:m-32`}>
+        <div className='flex flex-col transition-all lg:grid lg:grid-cols-12 lg:gap-8'>
+          <img
+            src={image}
+            alt={image_alt_text}
+            className={`${reversed && 'lg:order-1'}  z-[0] box-border w-screen overflow-visible object-cover lg:col-span-5 lg:row-span-1 lg:h-full lg:w-full`}
+          />
+          <div className='radius-3xl z-[1] flex h-full flex-col justify-between bg-neutral-3 py-8 text-center text-neutral-8 shadow-md transition-all lg:col-span-7 lg:px-6 lg:py-16 dark:bg-neutral-7 dark:text-neutral-2'>
+            <div>
+              <div className='flex flex-col items-center'>
                 <ReactMarkdown
-                  className='markdown mb-32 text-left lg:text-left'
                   components={{
                     p(props) {
                       const { node, ...rest } = props;
-                      return (
-                        <p className='h3-sub mb-2 text-[2.0rem]' {...rest} />
-                      );
+                      return <h2 className='' {...rest} />;
                     },
                   }}
+                  className=''
                 >
-                  {body_text}
+                  {heading}
                 </ReactMarkdown>
+                <div className='mb-16 h-[2px] w-3/4 max-w-2xl bg-border-gradient'></div>
+                <div className='px-12'>
+                  {bold_text && (
+                    <ReactMarkdown
+                      className='markdown text-left lg:text-left'
+                      components={{
+                        p(props) {
+                          const { node, ...rest } = props;
+                          return (
+                            <strong>
+                              <p
+                                className='h2-sub mb-12 text-center text-primary-5'
+                                {...rest}
+                              />
+                            </strong>
+                          );
+                        },
+                      }}
+                    >
+                      {bold_text}
+                    </ReactMarkdown>
+                  )}
+                  <ReactMarkdown
+                    className='markdown mb-32 text-left lg:text-left'
+                    components={{
+                      p(props) {
+                        const { node, ...rest } = props;
+                        return (
+                          <p className='h3-sub mb-2 text-[2.0rem]' {...rest} />
+                        );
+                      },
+                    }}
+                  >
+                    {body_text}
+                  </ReactMarkdown>
+                </div>
               </div>
             </div>
+            {link_text && (
+              <div>
+                {link_url && <Button url={link_url} text={link_text}></Button>}
+              </div>
+            )}
           </div>
-          {link_text && (
-            <div>
-              {link_url && (
-                <Button
-                  url={link_url}
-                  text={link_text}
-                ></Button>
-              )}
-            </div>
-          )}
         </div>
       </div>
     </section>
