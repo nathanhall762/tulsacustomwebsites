e35a68d25a150f9f56f8aae098062d5c4b6742dd
Nathan Hall <nathanhall762@gmail.com>
2024-11-19 17:04:16 -0600

    added to nav menu

--- src/navMenu.ts
+++ src/navMenu.ts
@@ -1,7 +1,7 @@
 export const navs = [
   { name: 'Home', link: '/' },
   { name: 'Our Work', link: '/our-work' },
-  // { name: 'Pricing', link: '/pricing' },
+  { name: 'Pricing', link: '/pricing' },
   { name: 'Contact Us', link: '/contact' },
   // { name: 'Page Sections', link: '/page-sections' },
 ];
